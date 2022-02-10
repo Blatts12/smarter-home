26bcda834648c26729ebaecfa5e2ecb7065bd219
Blatts12 <9402720+Blatts12@users.noreply.github.com>
2022-02-10 21:58:38 +0100

    update list on new fetched details

--- src/components/deviceWindow/DraggableDeviceWindow.tsx
+++ src/components/deviceWindow/DraggableDeviceWindow.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useQuery } from "react-query";
+import React, { useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
+import { SmartDevice } from "../../models/SmartDevice";
 import { SmartDeviceDetails } from "../../models/SmartDeviceDetails";
 import FetchDeviceDetails from "../../queries/FetchDeviceDetails";
 import { useUiStore } from "../../store/uiStore";
@@ -13,12 +14,30 @@ interface DraggableDeviceWindowProps {
 const DraggableDeviceWindow: React.FC<DraggableDeviceWindowProps> = ({
   selectedDevice,
 }) => {
+  const queryClient = useQueryClient();
   const closeWindow = useUiStore((state) => state.closeWindow);
   const { isLoading, data, error } = useQuery<SmartDeviceDetails>(
     ["device", selectedDevice],
     async () => FetchDeviceDetails.action(selectedDevice),
     FetchDeviceDetails.options
   );
+
+  useEffect(() => {
+    console.log("new");
+    if (data) {
+      console.log("new in 1");
+      const state = queryClient.getQueryState<SmartDevice[]>("devices");
+      const oldState = state?.data;
+      if (oldState) {
+        console.log("new in 2");
+        const deviceIndex = oldState.findIndex((dev) => dev.id === data.id);
+        oldState[deviceIndex] = data;
+        queryClient.setQueryData("devices", oldState);
+      }
+    }
+
+    return () => {};
+  }, [queryClient, data]);
 
   return (
     <div className="drag-device-area">
--- src/components/deviceWindow/FullDeviceWindow.tsx
+++ src/components/deviceWindow/FullDeviceWindow.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BsX } from "react-icons/bs";
 import { HiPencil } from "react-icons/hi";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
+import { SmartDevice } from "../../models/SmartDevice";
 import { SmartDeviceDetails } from "../../models/SmartDeviceDetails";
 import FetchDeviceDetails from "../../queries/FetchDeviceDetails";
 import { useUiStore } from "../../store/uiStore";
@@ -16,12 +17,30 @@ interface FullDeviceWindowProps {
 const FullDeviceWindow: React.FC<FullDeviceWindowProps> = ({
   selectedDevice,
 }) => {
+  const queryClient = useQueryClient();
   const closeWindow = useUiStore((state) => state.closeWindow);
   const { isLoading, data, error } = useQuery<SmartDeviceDetails>(
     ["device", selectedDevice],
     async () => FetchDeviceDetails.action(selectedDevice),
     FetchDeviceDetails.options
   );
+
+  useEffect(() => {
+    console.log("new");
+    if (data) {
+      console.log("new in 1");
+      const state = queryClient.getQueryState<SmartDevice[]>("devices");
+      const oldState = state?.data;
+      if (oldState) {
+        console.log("new in 2");
+        const deviceIndex = oldState.findIndex((dev) => dev.id === data.id);
+        oldState[deviceIndex] = data;
+        queryClient.setQueryData("devices", oldState);
+      }
+    }
+
+    return () => {};
+  }, [queryClient, data]);
 
   return (
     <div className="full-window">
